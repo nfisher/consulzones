a3eedf5699cd55b0a139446312eb68b7b4741416
Nathan Fisher <nfisher@junctionbox.ca>
2015-11-14 15:47:57 +0000

    Trial 2: External cluster.

--- zone5.sh
+++ zone5.sh
@@ -5,6 +5,5 @@ service iptables stop
 export GOMAXPROCS=2
 /vagrant/consul/consul agent -data-dir /var/consul/data \
     -bind 192.168.33.11 -node zone5.local \
-    -retry-join 192.168.33.12 -retry-interval 5s \
-    -server -bootstrap-expect 3 \
+    -server -bootstrap-expect 1 \
     > /vagrant/logs/zone5.log 2>&1 &
