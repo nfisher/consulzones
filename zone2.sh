97f369938bf141642a4c995c0844eab1d92c1979
Nathan Fisher <nfisher@junctionbox.ca>
2015-11-14 15:47:57 +0000

    Trial 2: External Cluster.

--- zone2.sh
+++ zone2.sh
@@ -7,7 +7,6 @@ export GOMAXPROCS=2
   -bind 192.168.33.12 -node zone2.local \
   -config-dir /vagrant/consul/consul.d \
   -retry-join 192.168.33.11 -retry-interval 5s \
-  -server -bootstrap-expect 3 \
   > /vagrant/logs/zone2.log 2>&1 &
 
 python -m SimpleHTTPServer > /dev/null 2> /dev/null &
